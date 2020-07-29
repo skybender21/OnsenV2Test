258521bb90af83df9d24d71e8a00cb0db0939c39
skybender21 <skybender21@gmail.com>
2020-07-29 19:14:54 +0900

    Project's Initial Commit

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import * as ons from 'onsenui';
+
+@Component({
+  selector: 'app',
+  template: require('./app.html'),
+  styles: [ require('./app.css') ]
+})
+export class MyApp {
+  constructor() {}
+
+  alert() {
+    ons.notification.alert('This is an Onsen UI alert notification.');
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,45 @@
+// Onsen UI Styling and Icons
+require('onsenui/css/onsen-css-components.css');
+require('onsenui/css/onsenui.css');
+
+import * as ons from 'onsenui';
+
+// Application code starts here
+import {enableProdMode, NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {HttpModule} from '@angular/http';
+import {OnsenModule} from 'ngx-onsenui';
+
+import {MyApp} from './app/app';
+
+// Enable production mode when in production mode.
+if (process.env.NODE_ENV === 'production') {
+  enableProdMode();
+}
+
+if (ons.platform.isIPhoneX()) {
+  document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
+  document.documentElement.setAttribute('onsflag-iphonex-landscape', '');
+}
+
+@NgModule({
+    imports: [
+        OnsenModule, // has BrowserModule internally
+        HttpModule,
+    ],
+    declarations: [
+        MyApp,
+    ],
+    bootstrap: [
+        MyApp,
+    ],
+    schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
+    ],
+})
+class AppModule {}
+
+if (module['hot']) module['hot'].accept();
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+.catch(err => console.error(err));
